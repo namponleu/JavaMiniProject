e4a16501b377b8a6f007bddff5c69b17327b4ab9
nam ponleu <ponleunam27@gmail.com>
2026-03-25 17:44:32 +0700

    Resolve merge confilct in model.service.StudentServiceImpl

--- src/fileIO/model/service/StudentServiceImpl.java
+++ src/fileIO/model/service/StudentServiceImpl.java
@@ -1,4 +1,7 @@
 package fileIO.model.service;
+
+
+
 import fileIO.controller.StudentController;
 import fileIO.model.Student;
 import java.io.*;
@@ -45,6 +48,7 @@ public class StudentServiceImpl implements StudentService {
             e.printStackTrace();
         }
     }
+
     private void writeDataToFile() {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME))) {
             writer.flush();
@@ -62,12 +66,15 @@ public class StudentServiceImpl implements StudentService {
             e.printStackTrace();
         }
     }
+
+
     @Override
     public int addNewStudent(Student student) {
         students.add(student);
         writeDataToFile();
         return students.size();
     }
+
     @Override
     public List<Student> listAllStudents() {
         return students;
@@ -78,6 +85,12 @@ public class StudentServiceImpl implements StudentService {
         writeDataToFile();
         System.out.println("Data committed to file successfully.");
     }
+
+//    @Override
+//    public void commitDataFromTransaction() {
+//
+//    }
+
     private void clearTransactionFile(String fileName) {
         File file = new File(fileName);
         try {
@@ -94,35 +107,54 @@ public class StudentServiceImpl implements StudentService {
             e.printStackTrace();
         }
     }
+
+
     @Override
     public void commitDataFromTransaction() {
         String transactionFileName = "src/allFile/TransactionFile.txt";
+
         if (Files.exists(Paths.get(transactionFileName))) {
             try (BufferedReader reader = new BufferedReader(new FileReader(transactionFileName))) {
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        try {
-                            // Process and add transaction data to main data
-                            String[] data = line.split(",");
-                            String id = data[0];
-                            String name = data[1];
-                            LocalDate dateOfBirth = LocalDate.parse(data[2]);
-                            String classroom = data[3];
-                            String subjects = data[4];
-                            LocalDate createdAt = LocalDate.parse(data[5]);
+//                System.out.print("Commit your pending data record(s) beforehand [Y/N]: ");
+//                String choice = scanner.nextLine().toUpperCase();
 
-                            Student student = new Student(id, name, dateOfBirth, classroom, subjects, createdAt);
-                            students.add(student);
-                        } catch (DateTimeParseException | ArrayIndexOutOfBoundsException e) {
-                            System.out.println("Error processing transaction data: " + e.getMessage());
-                        }
+//                if (choice.equals("Y")) {
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    try {
+                        // Process and add transaction data to main data
+                        String[] data = line.split(",");
+                        String id = data[0];
+                        String name = data[1];
+                        LocalDate dateOfBirth = LocalDate.parse(data[2]);
+                        String classroom = data[3];
+                        String subjects = data[4];
+                        LocalDate createdAt = LocalDate.parse(data[5]);
+
+                        Student student = new Student(id, name, dateOfBirth, classroom, subjects, createdAt);
+                        students.add(student);
+                    } catch (DateTimeParseException | ArrayIndexOutOfBoundsException e) {
+                        System.out.println("Error processing transaction data: " + e.getMessage());
                     }
-                    // Write the updated data to the main file
-                    writeDataToFile();
-                    System.out.println("Data committed successfully.");
-                    // Clear transaction file after committing data
-                    clearTransactionFile(transactionFileName);
-                    StudentController.start();
+                }
+
+                // Write the updated data to the main file
+                writeDataToFile();
+                System.out.println("Data committed successfully.");
+
+                // Clear transaction file after committing data
+                clearTransactionFile(transactionFileName);
+
+                // Import and call displayTitle() and displayMenu() from StudentController
+//                    StudentController.displayTitle();
+//                    StudentController.displayMenu();
+                StudentController.start();
+//                } else if (choice.equals("N")) {
+//                    System.out.println("Operation canceled.");
+//                } else {
+//                    System.out.println("Invalid choice. Please enter Y or N.");
+//                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -130,6 +162,8 @@ public class StudentServiceImpl implements StudentService {
             System.out.println("No transaction file found.");
         }
     }
+
+
     @Override
     public List<Student> searchStudentById(String id) {
         List<Student> result = new ArrayList<>();
@@ -140,6 +174,7 @@ public class StudentServiceImpl implements StudentService {
         }
         return result;
     }
+
     @Override
     public List<Student> searchStudentByName(String name) {
         List<Student> result = new ArrayList<>();
@@ -150,6 +185,7 @@ public class StudentServiceImpl implements StudentService {
         }
         return result;
     }
+
     @Override
     public Student deleteStudentById(String id) {
         Student studentToRemove = null;
@@ -165,6 +201,7 @@ public class StudentServiceImpl implements StudentService {
         }
         return studentToRemove;
     }
+
     @Override
     public Student updateStudentById(String id, Student updatedStudent) {
         try {
@@ -191,11 +228,13 @@ public class StudentServiceImpl implements StudentService {
             return null;
         }
     }
+
     @Override
     public void deleteAllStudents() {
         students.clear();
         writeDataToFile();
     }
+
     @Override
     public void generateRecords(int startIndex, int endIndex) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME, true))) {
@@ -203,7 +242,7 @@ public class StudentServiceImpl implements StudentService {
                 // Generate student data (ID, name, date of birth, etc.)
                 String id = generateDefaultId();
                 String name = "Student" + (i + 1);
-                LocalDate dateOfBirth = LocalDate.of(2000 + i % 20, (i % 12) + 1, (i % 28) + 1); // Random date of birth
+                LocalDate dateOfBirth = LocalDate.of(2000 + i % 20, (i % 12) + 1, (i % 28) + 1);
                 String classroom = "Class" + (i % 5 + 1);
                 String subjects = "Subject" + (i % 8 + 1);
                 LocalDate createAt = LocalDate.now(); // Corrected creation date
